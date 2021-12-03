32a2cd3bfefa31461aa6528776081291ddda5e8f
Treeaza <jasper.rubin@gmail.com>
2021-12-03 17:07:14 -0330

    Change logo color and background.

---
+++ logo.js
@@ -0,0 +1,19 @@
+let logoFiles = ["logo1.png", "logo2.png", "logo3.png", "logo4.png"];
+
+const LOGO = document.querySelector("#logo")
+
+var index = 0;
+
+var ticker = setInterval(function () {
+	LOGO.src = logoFiles[index];
+
+	console.log(index);
+	console.log(logoFiles[index]);
+
+	index++;
+
+	if (index >= logoFiles.length) {
+		index = 0;
+	}
+
+}, 800);
